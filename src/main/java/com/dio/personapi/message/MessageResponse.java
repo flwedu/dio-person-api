f604d8950640a3767ffbb5d2572da662e7b1a177
Eduardo Aquino <55285816+flwedu@users.noreply.github.com>
2021-07-19 20:47:01 -0300

    Class Message created

---
+++ src/main/java/com/dio/personapi/message/MessageResponse.java
@@ -0,0 +1,15 @@
+package com.dio.personapi.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class MessageResponse {
+
+    private String message;
+}
