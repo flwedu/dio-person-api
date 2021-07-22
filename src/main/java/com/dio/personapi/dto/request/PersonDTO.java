b11b338313bf592ca92429448aa2e15982f52e80
Eduardo Aquino <55285816+flwedu@users.noreply.github.com>
2021-07-22 17:48:20 -0300

    organize imports

--- src/main/java/com/dio/personapi/dto/request/PersonDTO.java
+++ src/main/java/com/dio/personapi/dto/request/PersonDTO.java
@@ -1,16 +1,17 @@
 package com.dio.personapi.dto.request;
 
-import com.dio.personapi.model.Phone;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.br.CPF;
+import java.util.List;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-import java.util.List;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
